6ee9695e44512dd5a91ca75e3ca8c254ebce82f5
Ivan Balashov <sputnic1985@gmail.com>
2015-10-01 23:06:36 -0400

    Initial commit

---
+++ app/src/main/java/com/firstsputnik/beatbox/Sound.java
@@ -0,0 +1,25 @@
+package com.firstsputnik.beatbox;
+
+/**
+ * Created by ibalashov on 10/1/2015.
+ */
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String fileName = components[components.length - 1];
+        mName = fileName.replace(".wav", "");
+    }
+
+    public String getAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
